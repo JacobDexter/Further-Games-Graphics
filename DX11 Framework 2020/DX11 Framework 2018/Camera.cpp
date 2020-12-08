1ec0f8084463b7a8cce693aa1b56d3f469916eac
Jacob Dexter <57293065+JacobDexter@users.noreply.github.com>
2020-12-08 20:54:18 +0000

    Camera Class

--- DX11 Framework 2020/DX11 Framework 2018/Camera.cpp
+++ DX11 Framework 2020/DX11 Framework 2018/Camera.cpp
@@ -1,8 +1,20 @@
 #include "Camera.h"
 
-Camera::Camera(XMFLOAT3 position, XMFLOAT3 at, XMFLOAT3 up, FLOAT nearDepth, float farDepth)
+Camera::Camera(XMFLOAT3 position, XMFLOAT3 at, XMFLOAT3 up, FLOAT windowHeight, FLOAT windowWidth, FLOAT nearDepth, FLOAT farDepth)
 {
+	_eye = position;
+	_at = at;
+	_up = up;
 
+	_windowHeight = windowHeight;
+	_windowWidth = windowWidth;
+	_nearDepth = nearDepth;
+	_farDepth = farDepth;
+
+	// Initialize the view matrix
+	XMStoreFloat4x4(&_view, XMMatrixLookAtLH(GetPositionXM(), GetLookAtXM(), GetUpXM()));
+	// Initialize the projection matrix
+	XMStoreFloat4x4(&_projection, XMMatrixPerspectiveFovLH(XM_PIDIV2, _windowWidth / _windowHeight, 0.01f, 100.0f));
 }
 
 Camera::~Camera()
@@ -11,6 +23,82 @@ Camera::~Camera()
 }
 
 void Camera::Update()
+{
+
+}
+
+///////////////////////
+///////////////////////
+///////////////////////
+
+XMFLOAT3 Camera::GetPosition()
+{
+	return _eye;
+}
+
+XMVECTOR Camera::GetPositionXM()
+{
+	return XMLoadFloat3(&_eye);
+}
+
+void Camera::SetPosition(float x, float y, float z)
+{
+	_eye = { x, y, z };
+}
+
+XMFLOAT3 Camera::GetLookAt()
+{
+	return _at;
+}
+
+XMVECTOR Camera::GetLookAtXM()
+{
+	return XMLoadFloat3(&_at);
+}
+
+void Camera::SetLookAt(float x, float y, float z)
+{
+	_at = { x, y, z };
+}
+
+XMFLOAT3 Camera::GetUp()
+{
+	return _up;
+}
+
+XMVECTOR Camera::GetUpXM()
+{
+	return XMLoadFloat3(&_up);
+}
+
+void Camera::SetUp(float x, float y, float z)
+{
+	_up = { x, y, z };
+}
+
+///////////////////////
+///////////////////////
+///////////////////////
+
+XMFLOAT4X4 Camera::GetViewMx()
+{
+	return _view;
+}
+
+XMFLOAT4X4 Camera::GetProjectionMx()
+{
+	return _projection;
+}
+
+XMFLOAT4X4 Camera::GetViewProjectionMx()
+{
+	XMFLOAT4X4 viewProjection;
+	XMStoreFloat4x4(&viewProjection, XMMatrixMultiply(XMLoadFloat4x4(&GetProjectionMx()), XMLoadFloat4x4(&GetViewMx()))); //view projection
+
+	return viewProjection;
+}
+
+void Camera::Reshape(FLOAT windowWidth, FLOAT windowHeight, FLOAT nearDepth, FLOAT farDepth)
 {
 
 }
--- DX11 Framework 2020/DX11 Framework 2018/Camera.h
+++ DX11 Framework 2020/DX11 Framework 2018/Camera.h
@@ -3,7 +3,6 @@
 #include <d3d11_1.h>
 #include <d3dcompiler.h>
 #include <directxmath.h>
-#include "Constants.h"
 
 using namespace DirectX;
 
@@ -14,14 +13,33 @@ private:
 	XMFLOAT3 _at;
 	XMFLOAT3 _up;
 
+	FLOAT _windowHeight;
+	FLOAT _windowWidth;
 	FLOAT _nearDepth;
 	FLOAT _farDepth;
 
 	XMFLOAT4X4 _view;
 	XMFLOAT4X4 _projection;
 public:
-	Camera(XMFLOAT3 position, XMFLOAT3 at, XMFLOAT3 up, FLOAT nearDepth, float farDepth);
+	Camera(XMFLOAT3 position, XMFLOAT3 at, XMFLOAT3 up, FLOAT windowHeight, FLOAT windowWidth, FLOAT nearDepth, FLOAT farDepth);
 	~Camera();
 
 	void Update();
+
+	XMFLOAT3 GetPosition();
+	XMVECTOR GetPositionXM();
+	XMFLOAT3 GetLookAt();
+	XMVECTOR GetLookAtXM();
+	XMFLOAT3 GetUp();
+	XMVECTOR GetUpXM();
+
+	void SetPosition(float x, float y, float z);
+	void SetLookAt(float x, float y, float z);
+	void SetUp(float x, float y, float z);
+
+	XMFLOAT4X4 GetViewMx();
+	XMFLOAT4X4 GetProjectionMx();
+	XMFLOAT4X4 GetViewProjectionMx();
+
+	void Reshape(FLOAT windowWidth, FLOAT windowHeight, FLOAT Depth, FLOAT farDepth);
 };
